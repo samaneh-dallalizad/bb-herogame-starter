b6024d01f3b8602211973081143bfbd605e6541e
Samaneh Dallalizad <samaneh.dallalizad@gmail.com>
2019-04-27 22:29:19 +0200

    finish section 1-3

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,28 @@
 // Write your JS here
+let hero={
+    name:"samaneh",
+    heroic:true,
+    inventory:[],
+    health:10,
+    weapon:{
+        type:"",
+        damage:2
+    }
+}
+
+function rest(param){
+    if(param.health === 10){
+        alert("oops");
+    }
+    param.health = 10;    
+    return param;
+}
+function pickUpItem(hero, weapon) {
+    hero.inventory.push(weapon);
+}
+function equipWeapon(hero) {
+    if(hero.inventory.length===0){
+        return;
+    }
+    hero.weapon=hero.inventory[0];
+}
